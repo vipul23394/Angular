1b5f1d9e21ec4511c39a96b2d840314b6a09eb2f
vipul <vipul23394gmail.com>
2020-01-27 09:06:14 +0530

    New changes on data file

--- data.js
+++ data.js
@@ -1,33 +1,28 @@
+module.exports = {
 
-module.exports=
-	{
+	datadriven :
 
-datadriven:
-	
 	{
 
-userdata1:
-	{
-		name:"vipul pandey",
-		email:"vipul10310@gmail.com",
-		pass:"vipul10310@gmail",
-	
-	},
-	
-	userdata2:
-	{
-		name:"Surya",
-		email:"vipul10310@gmail.com",
-		pass:"vipul10310@gmail",
-	
-	},
-	
-	userdata3:
-	{
-		name:"vipul",
-		email:"vipul10310@gmail.com",
-		pass:"vipul10310@gmail",
-	
+		userdata1 : {
+			name : "vipul pandey",
+			email : "vipul10310@gmail.com",
+			pass : "vipul10310@gmail",
+
+		},
+
+		userdata2 : {
+			name : "Surya",
+			email : "vipul10310@gmail.com",
+			pass : "vipul10310@gmail",
+
+		},
+
+		userdata3 : {
+			name : "vipul",
+			email : "vipul10310@gmail.com",
+			pass : "vipul10310@gmail",
+
+		}
 	}
-}
 }
